1c105f8bb9e889a5a83eb88567eb0caf440c2cb7
Ofek yehuda <ofekyehuda@gmail.com>
2024-08-09 18:06:33 +0300

    added http status codes and included the 200 OK in all the endpoints

--- Presentations/presentationsAPI.js
+++ Presentations/presentationsAPI.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { StatusCodes } = require("http-status-codes");
 
 const presentationsRouter = express.Router();
 const PresentationsBL = require("./presentationsBL");
@@ -6,7 +7,7 @@ const PresentationsBL = require("./presentationsBL");
 // Get all presentations
 presentationsRouter.get('/', async (req, res) => {
     const allPresentations = await PresentationsBL.getAllPresentations();
-    res.json(allPresentations);
+    res.status(StatusCodes.OK).json(allPresentations);
 });
 
 // Creating a new presentation
@@ -18,25 +19,25 @@ presentationsRouter.post('/', async (req, res) => {
     };
 
     const newPresentation = await PresentationsBL.createPresentation(presentation);
-    res.json(newPresentation);
+    res.status(StatusCodes.OK).json(newPresentation);
 });
 
 // Deleting a presentation by its title
 presentationsRouter.delete('/:title', async (req, res) => {
     const isDeleted = await PresentationsBL.deletePresentation(req.params.title);
-    res.send(isDeleted);
+    res.status(StatusCodes.OK).send(isDeleted);
 });
 
 // Fetching a Presentation by title 
 presentationsRouter.get('/:title', async (req, res) => {
     const presentation = await PresentationsBL.getPresentationByTitle(req.params.title);
-    res.send(presentation);
+    res.status(StatusCodes.OK).send(presentation);
 });
 
 // Altering the Authors List
 presentationsRouter.patch('/:title', async (req, res) => {
     const updatedPresentation = await PresentationsBL.updateAuthorList(req.params.title, req.body.authorsList);
-    res.send(updatedPresentation);
+    res.status(StatusCodes.OK).send(updatedPresentation);
 });
 
 module.exports = {
--- Slides/slidesAPI.js
+++ Slides/slidesAPI.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const slidesRouter = express.Router({ mergeParams: true })
+const slidesRouter = express.Router({ mergeParams: true });
+const { StatusCodes } = require("http-status-codes");
 
 const SlidesBL = require("./slidesBL");
 
 // Creating a slide
 slidesRouter.post('/', async (req, res) => {
     const slide = await SlidesBL.createSlide(req.params.presentationTitle, req.body);
-    res.json(slide);
+    res.status(StatusCodes.OK).json(slide);
 })
 
 // Deleting a slide by its index
 slidesRouter.delete('/:index', async (req, res) => {
     const isDeleted = await SlidesBL.deleteSlide(req.params.presentationTitle, req.params.index)
-    res.send(isDeleted)
+    res.status(StatusCodes.OK).send(isDeleted)
 })
 
 // Altering a Slide 
 slidesRouter.put('/:index', async (req, res) => {
     const slide = await SlidesBL.updateSlide(req.params.presentationTitle, req.params.index, req.body)
 
-    res.send(slide)
+    res.status(StatusCodes.OK).send(slide)
 })
 
 module.exports = {
